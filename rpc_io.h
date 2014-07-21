896cc20046df08ffe8207c01ea50d1dc4b0a7ed2
Daniel Pitzl <daniel.pitzl@desy.de>
2014-07-21 10:41:28 +0200

    header cosmetics

--- rpc_io.h
+++ rpc_io.h
@@ -2,14 +2,17 @@
 #pragma once
 
 #include "rpc_error.h"
+#include <iostream> // cout
 
-
-class CRpcIo {
-protected:
+class CRpcIo
+{
+ protected:
   void Dump( const char *msg, const void *buffer, unsigned int size );
-public:
-    virtual ~ CRpcIo(  ) {
-  } virtual void Write( const void *buffer, unsigned int size ) = 0;
+ public:
+  virtual ~CRpcIo(  )
+    {
+    }
+  virtual void Write( const void *buffer, unsigned int size ) = 0;
   virtual void Flush(  ) = 0;
   virtual void Clear(  ) = 0;
   virtual void Read( void *buffer, unsigned int size ) = 0;
@@ -18,20 +21,31 @@ public:
 
 // usb.h implements CRpcIo as CUSB
 
-// Null ?
+// Null ? This should not be called
 
-class CRpcIoNull:public CRpcIo {
-public:
-  void Write( const void *buffer, unsigned int size ) {
+class CRpcIoNull:public CRpcIo
+{
+ public:
+  void Write( const void *buffer, unsigned int size )
+  {
+    std::cout << "rpc_io Null called for Write " << size
+	      << " at " << buffer << std::endl;
     throw CRpcError( CRpcError::WRITE_ERROR );
-  } void Flush(  ) {
   }
-  void Clear(  ) {
+  void Flush(  )
+  {
   }
-  void Read( void *buffer, unsigned int size ) {
+  void Clear(  )
+  {
+  }
+  void Read( void *buffer, unsigned int size )
+  {
+    std::cout << "rpc_io Null called for Read " << size
+	      << " at " << buffer << std::endl;
     throw CRpcError( CRpcError::READ_ERROR );
   } // real Read is in usb.h
-  void Close(  ) {
+  void Close(  )
+  {
   }
 
 };
